1819982405085233e2af1959409f528368ecfffd
sagarhursale <rsagar.hursale143@gmail.com>
2026-03-31 15:04:25 +0530

    qd

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "instance_name" {
-  value = aws_instance.myinstance.tags["Name"]
+  value = aws_instance.myinstance[*].tags["Name"]
 }
-output "instance_ip" {
-  value = aws_instance.myinstance.public_ip
 
+output "instance_ip" {
+  value = aws_instance.myinstance[*].public_ip
 }
