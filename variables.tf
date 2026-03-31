6eaf87ab98fddf99b12cacb549f89d66667eb5ea
sagarhursale <rsagar.hursale143@gmail.com>
2026-03-31 14:39:47 +0530

    ssq

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "myami" {
   description = "print your region specfic ami id"
+  default = "ami-05d2d839d4f73aafb"
 }
 
 variable "myinstance" {
   description = "provide type of your instance"
+  default = "t3.micro"
 
 
 }
